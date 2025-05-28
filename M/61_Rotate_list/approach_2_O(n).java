9d649802327d5619a481219972881558eea89d42
GauravMittal06 <gauravchandra.raj@gmail.com>
2025-05-28 16:41:29 +0530

    Update approach_2_O(n).java

--- M/61_Rotate_list/approach_2_O(n).java
+++ M/61_Rotate_list/approach_2_O(n).java
@@ -1,1 +1,27 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0)
+        {
+            return head;
+        }
+        ListNode tail=head;
+        int c=1;
+        while(tail.next!=null)
+        {
+            tail=tail.next;
+            c++;
+        }
+        tail.next=head; //formed the circular list
+        k=k%c; //effective number of rotations
+        int target_tail = c-k-1;
+        ListNode newTail = head; //initialized the new tail as head, and will traverse it to the 'target_tail' index
 
+        for(int i=0;i<target_tail;i++)
+        {
+            newTail=newTail.next;
+        } //newTail reached the desired location
+        ListNode newHead = newTail.next; //the next index to 'newTail' will be the head
+        newTail.next=null; //break the next pointer of the newTail to make it a 'Tail'
+        return newHead;
+    }
+}
