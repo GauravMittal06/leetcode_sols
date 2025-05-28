eb5e7b99e9ce7cd83efe2c14b8fa8148297a83aa
GauravMittal06 <gauravchandra.raj@gmail.com>
2025-05-28 16:34:50 +0530

    Update approach_1_O(k*n).java

--- M/61_Rotate_list/approach_1_O(k*n).java
+++ M/61_Rotate_list/approach_1_O(k*n).java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0)
+        {
+            return head;
+        }
+        ListNode curr=head;
+        ListNode next=curr.next;
+        ListNode count=head;
+        int c=1;
+        while(count.next!=null)
+        {
+            count=count.next;
+            c++;
+        }
+        if(k>=c)
+        {
+            k=k%c;
+        }
+        for(int i=1;i<=k;i++)
+        {
+            while(next.next!=null)
+            {
+                curr=curr.next;
+                next=next.next;
+            }
+            curr.next=null;
+            next.next=head;
+            head=next;
+            curr=head;
+            next=next.next;
+        }
+        return head;
+    }
+}
