5115b53f6d423f63b13888a2685c0e78c842f859
GauravMittal06 <gauravchandra.raj@gmail.com>
2025-11-01 16:34:14 +0530

    Create binary_search_insert_approach.java

---
+++ E/35_Search_insert_position/binary_search_insert_approach.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int mid=0;
+            //if target present
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+            //if target is not present
+        return right+1;
+    }
+}
