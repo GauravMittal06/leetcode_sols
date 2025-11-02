59327d2979ec94f1ac8e67d9794d33888c1b910c
GauravMittal06 <gauravchandra.raj@gmail.com>
2025-11-02 20:20:03 +0530

    Update O(N.logN).java

--- M/1985_find_the_Kth_largest_integer_in_the_array/O(N.logN).java
+++ M/1985_find_the_Kth_largest_integer_in_the_array/O(N.logN).java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.math.BigInteger;
 class Solution {
     public String kthLargestNumber(String[] nums, int k) {
-        Arrays.sort(nums, (a, b) -> new BigInteger(a).compareTo(new BigInteger(b)));
+        Arrays.sort(nums, (a, b) -> new BigInteger(a).compareTo(new BigInteger(b))); //using BigInteger for using large (18+) digits
         return String.valueOf(nums[nums.length-k]);
     }
 }
